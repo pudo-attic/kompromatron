ec73f8c8b62cceefb856368d19594092765a3e33
Friedrich Lindenberg <friedrich@pudo.org>
2014-02-22 17:20:12 +0100

    Basic browser.

--- kompromatron/views/base.py
+++ kompromatron/views/base.py
@@ -1,10 +1,31 @@
 from flask import Blueprint, render_template, request
 # from flask import redirect
 
-from kompromatron.core import grano
+from kompromatron.core import grano, app
 
 
 base = Blueprint('base', __name__, static_folder='../static', template_folder='../templates')
+SCHEMA_CACHE = {}
+
+
+def get_schema(name):
+    if not name in SCHEMA_CACHE:
+        SCHEMA_CACHE[name] = grano.schemata.by_name(name)
+    return SCHEMA_CACHE[name]
+
+
+def get_attribute(schemata, name):
+    if not isinstance(schemata, list):
+        schemata = [schemata]
+    for schema in schemata:
+        schema = get_schema(schema.get('name'))
+        for attribute in schema.attributes:
+            if attribute.get('name') == name:
+                return attribute
+
+@app.context_processor
+def inject():
+    return dict(get_schema=get_schema, get_attribute=get_attribute)
 
 
 @base.route('/')
@@ -15,7 +36,15 @@ def index():
 @base.route('/entities/<id>')
 def entity(id):
     entity = grano.entities.by_id(id)
-    return render_template('entity.html', entity=entity)
+    schemata = entity.schemata
+    return render_template('entity.html', entity=entity, schemata=schemata)
+
+
+@base.route('/relations/<id>')
+def relation(id):
+    relation = grano.relations.by_id(id)
+    return render_template('relation.html', relation=relation)
+
 
 
 @base.route('/browse')
