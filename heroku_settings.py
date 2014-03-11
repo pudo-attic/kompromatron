746df42ff459c52690a5cf8c786a6d91edee7151
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2014-03-11 11:41:30 +0100

    Make heroku debug settings environ dependent

--- heroku_settings.py
+++ heroku_settings.py
@@ -1,7 +1,7 @@
 import os
 
-DEBUG = True
-ASSETS_DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+ASSETS_DEBUG = os.environ.get('ASSET_DEBUG', 'False') == 'True'
 
 # GRANO_HOST = 'http://localhost:5000'
 # GRANO_APIKEY = '7a65f180d7b898822'
