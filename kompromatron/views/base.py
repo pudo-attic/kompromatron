953e7d1221657caaf0cd1b46d1eba02aba5df165
Friedrich Lindenberg <friedrich@pudo.org>
2014-03-14 11:55:00 -0300

    fix search?

--- kompromatron/views/base.py
+++ kompromatron/views/base.py
@@ -56,7 +56,7 @@ def browse():
         'offset': request.args.get('offset', 0),
         'project': grano.slug
     }
-    s, results = grano.client.get('/entities', params=params)
+    s, results = grano.client.get('/entities/_search', params=params)
     #print results.get('results')[0]
     return render_template('browse.html', results=results,
         query=params.get('q'))
